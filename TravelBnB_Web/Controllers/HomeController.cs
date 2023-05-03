077d090b74bae9131761aca69540298f1173af02
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-05-04 01:13:30 +0200

    style home

--- TravelBnB_Web/Controllers/HomeController.cs
+++ TravelBnB_Web/Controllers/HomeController.cs
@@ -1,32 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using TravelBnB_Web.Models;
+using TravelBnB_Web.Services.IServices;
 
 namespace TravelBnB_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IMapper _mapper;
+        private readonly IApartmentService _apartmentService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IApartmentService apartmentService,IMapper mapper)
         {
-            _logger = logger;
+            _apartmentService = apartmentService;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ApartmentDTO> listApts = new List<ApartmentDTO>();
+            var response = await _apartmentService.GetAllAsync<APIResponse>();
+            if(response is not null && response.IsSuccess)
+            {
+                listApts = JsonConvert.DeserializeObject<List<ApartmentDTO>>(Convert.ToString(response.Result));
+            }
+            return View(listApts);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
